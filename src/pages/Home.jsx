7f39af3df007a63521f920abb8128fa1456ca4b1
Freskkie Encarnacion <fresenc112233@gmail.com>
2025-09-09 13:42:08 +0800

    add home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HomeSection } from "../components/HomeSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
 
 export const Home = () => {
     return (
@@ -15,6 +20,13 @@ export const Home = () => {
             <Navbar />
 
             {/* Main Content */}
+            <main>
+                <HomeSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <ContactSection />
+            </main>
 
             {/* Footer */}
         </div>
