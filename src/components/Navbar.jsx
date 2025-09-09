983b61f2b1b783ab795aac8cd8a8422afddeddb3
Freskkie Encarnacion <fresenc112233@gmail.com>
2025-09-09 13:41:44 +0800

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Menu, X } from "lucide-react";
 
 const navItems = [
-    {name: "Home", href: "#hero"},
+    {name: "Home", href: "#home"},
     {name: "About", href: "#about"},
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
